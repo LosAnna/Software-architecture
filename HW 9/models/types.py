f256401509aa9c43e930ef803a63d1ed4b0e4dbc
losanna <79831340359@yandex.ru>
2023-12-29 21:59:07 +0700

    Home work 9

---
+++ HW 9/models/types.py
@@ -0,0 +1,11 @@
+from pydantic import BaseModel
+from enum import Enum
+
+class TemperatureFormat(Enum):
+    CELSIUS = 'c'
+    FAHRENHEIT = 'f'
+
+class Temperature(BaseModel):
+    time: float
+    temperature: float
+    param: TemperatureFormat
